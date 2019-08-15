ccff1b6de72664fd913d3d80077df2f995104c23
Victorplopez <victorplopez@yahoo.com>
2019-08-14 19:52:50 -0700

    playing with primatives, and output statements

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// var firstName = 'Victor';
+// var age = 26;
+
+// console.log(firstName.length);
+// const firstName = 'Victor';
+
+// Old way
+// console.log('My name is ' + firstName + ' and my age is ' + age);
+
+// Better way
+// console.log(`My name is ${firstName} and my age is ${age}`);
+
+// alert(`Hello, I am ${firstName}`);
+
+// const birthday = '01/07/1993';
+// let fixDate = new Date(birthday);
+// console.log(birthday);
+
+// let hello = new String('Hello, there.');
+// alert(hello);
+
